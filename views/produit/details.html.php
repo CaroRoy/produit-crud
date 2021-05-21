0a30646f0e057364187206bfd7a9c4ded69d290d
CaroRoy <80893100+CaroRoy@users.noreply.github.com>
2021-05-21 16:45:16 +0200

    Update details.html.php

--- views/produit/details.html.php
+++ views/produit/details.html.php
@@ -27,7 +27,7 @@
                 </div>
             </div>
         </div>
-        <a class="btn btn-danger mt-5" href="http://localhost/CoursPHP/php_web/produit-crud/router.php/catalogue">Retour</a>
+        <a class="btn btn-dark mt-5" href="http://localhost/CoursPHP/php_web/produit-crud/router.php/catalogue">Retour</a>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
